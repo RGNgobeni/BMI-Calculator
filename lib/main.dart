329ad0c78e571d181c9264111da938b7b3721340
RGNgobeni <rgngobeni1@gmail.com>
2023-05-24 15:19:36 +0200

    updates on the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 88.0;
 const cardColour = Color(0xFF1D1E33);
@@ -32,9 +33,19 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
         Expanded(child: Row(
           children: <Widget>[
-            Expanded(child:ReusableCard(Color(0xFF1D1E33)),
+            Expanded(child:ReusableCard(Color(0xFF1D1E33),
+            cardChild: IconContent(
+              icon: FontAwesomeIcons.mars,
+               label: 'MALE',
+               ),
             ),
-            Expanded(child: ReusableCard(Color(0xFF1D1E33)),
+            ),
+            Expanded(child: ReusableCard(Color(0xFF1D1E33)
+            cardChild: IconContent(
+              icon: FontAwesomeIcons.venus,
+               label: 'FEMALE',
+               ),
+            ),
             ),
           ],
         ),
@@ -71,20 +82,5 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class ReusableCard extends StatelessWidget {
 
-  ReusableCard(this.colour);
-  final Color colour;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-    
-     margin: EdgeInsets.all(15.0),
-     decoration: BoxDecoration(
-      color: Color(0xFF1D1E33),
-      borderRadius: BorderRadius.circular(10.0),
-     ),
 
-       );
-  }
-}
