cd14b22597596ee210ed9ce81259c4475fb7084f
RGNgobeni <rgngobeni1@gmail.com>
2023-05-24 14:15:44 +0200

    updated code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 88.0;
+const cardColour = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
@@ -42,14 +46,26 @@ class _InputPageState extends State<InputPage> {
 
         Expanded(child: Row(
           children: <Widget>[
-            Expanded(child: ReusableCard(Color(0xFF1D1E33)),
+            Expanded(child: ReusableCard(Color(0xFF1D1E33)
             ),
-            Expanded(child: ReusableCard(Color(0xFF1D1E33)),
+            ),
+            Expanded(
+              child: ReusableCard(Color(0xFF1D1E33)
+            ),
        ),
           ],
-        ),),
-        ]
-      )
+        )
+        ),
+        Container(
+          color: bottomContainerColour,
+          margin: EdgeInsets.only(top: 10.0),
+          width: double.infinity,
+          height: bottomContainerHeight,
+          ),
+        
+        ],
+      
+      ),
 
     );
   }
